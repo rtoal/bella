187ca149f944474b4db9a93b3a6ddbb53ea30b64
Ray Toal <rtoal@lmu.edu>
2022-02-03 09:21:16 -0800

    Simplify parser with _iter rule

--- src/core.js
+++ src/core.js
@@ -137,8 +137,8 @@ Program.prototype[util.inspect.custom] = function () {
   function* lines() {
     function view(e) {
       if (tags.has(e)) return `#${tags.get(e)}`
-      if (e && e.constructor === Token) {
-        return e.category !== "#SYMBOL" ? e.lexeme : util.inspect(e.lexeme)
+      if (e?.constructor === Token) {
+        return `${e.category}(${e.lexeme})`
       }
       if (Array.isArray(e)) return `[${e.map(view)}]`
       return util.inspect(e)
--- src/parser.js
+++ src/parser.js
@@ -6,20 +6,16 @@ const bellaGrammar = ohm.grammar(fs.readFileSync("src/bella.ohm"))
 
 const astBuilder = bellaGrammar.createSemantics().addOperation("ast", {
   Program(body) {
-    return new core.Program(body.children.map(s => s.ast()))
+    return new core.Program(body.ast())
   },
   Statement_vardec(_let, id, _eq, initializer, _semicolon) {
-    return new core.VariableDeclaration(id.sourceString, initializer.ast())
+    return new core.VariableDeclaration(id.ast(), initializer.ast())
   },
   Statement_fundec(_fun, id, _open, params, _close, _equals, body, _semicolon) {
-    return new core.FunctionDeclaration(
-      id.sourceString,
-      params.asIteration().children.map(param => param.ast()),
-      body.ast()
-    )
+    return new core.FunctionDeclaration(id.ast(), params.asIteration().ast(), body.ast())
   },
   Statement_assign(id, _eq, expression, _semicolon) {
-    return new core.Assignment(id.sourceString, expression.ast())
+    return new core.Assignment(id.ast(), expression.ast())
   },
   Statement_print(_print, argument, _semicolon) {
     return new core.PrintStatement(argument.ast())
@@ -28,8 +24,7 @@ const astBuilder = bellaGrammar.createSemantics().addOperation("ast", {
     return new core.WhileStatement(test.ast(), body.ast())
   },
   Block(_open, body, _close) {
-    // No need for a block node, just return the list of statements
-    return body.children.map(s => s.ast())
+    return body.ast()
   },
   Exp_negation(op, operand) {
     return new core.NegationExpression(op.sourceString, operand.ast())
@@ -53,10 +48,10 @@ const astBuilder = bellaGrammar.createSemantics().addOperation("ast", {
     return new core.NotExpression(op.sourceString, operand.ast())
   },
   Test_or(left, _ops, right) {
-    return new core.OrExpression([left, ...right.children].map(e => e.ast()))
+    return new core.OrExpression([left.ast(), ...right.ast()])
   },
   Test_and(left, _ops, right) {
-    return new core.AndExpression([left, ...right.children].map(e => e.ast()))
+    return new core.AndExpression([left.ast(), ...right.ast()])
   },
   Condition_relation(left, op, right) {
     return new core.BinaryExpression(op.sourceString, left.ast(), right.ast())
@@ -65,29 +60,27 @@ const astBuilder = bellaGrammar.createSemantics().addOperation("ast", {
     return condition.ast()
   },
   Call(callee, _left, args, _right) {
-    return new core.Call(
-      callee.ast(),
-      args.asIteration().children.map(arg => arg.ast())
-    )
+    return new core.Call(callee.ast(), args.asIteration().ast())
   },
   id(_first, _rest) {
-    return new core.Token("#ID", this.sourceString, this.source.startIdx)
+    return new core.Token("Id", this.sourceString, this.source.startIdx)
   },
   true(_) {
-    return new core.Token("#BOOL", this.sourceString, this.source.startIdx)
+    return new core.Token("Bool", this.sourceString, this.source.startIdx)
   },
   false(_) {
-    return new core.Token("#BOOL", this.sourceString, this.source.startIdx)
+    return new core.Token("Bool", this.sourceString, this.source.startIdx)
   },
   num(_whole, _point, _fraction, _e, _sign, _exponent) {
-    return new core.Token("#NUM", this.sourceString, this.source.startIdx)
+    return new core.Token("Num", this.sourceString, this.source.startIdx)
+  },
+  _iter(...children) {
+    return children.map(child => child.ast())
   },
 })
 
 export default function parse(sourceCode) {
   const match = bellaGrammar.match(sourceCode)
-  if (!match.succeeded()) {
-    throw new Error(match.message)
-  }
+  if (!match.succeeded()) core.error(match.message)
   return astBuilder(match).ast()
 }
